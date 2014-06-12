7907cc77d6c4ad33654a55a1178cc80953c2f49b
Michael Garate <michaelgarate@trywildcard.com>
2014-06-12 12:18:49 -0400

    adjust schema attributes

--- src/test/java/com/wildcard/model/ProductCardBuilderTest.java
+++ src/test/java/com/wildcard/model/ProductCardBuilderTest.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,9 +24,7 @@ public class ProductCardBuilderTest {
     final String brand = "ItemsByLisa";
     
     // extensive attributes
-    // TODO: list of offers
     final String merchant = "Etsy";
-    final String gender = "female";
     final List<ProductColor> colors = new ArrayList<ProductColor>();
     final List<String> images = Arrays
             .asList("http://img0.etsystatic.com/017/0/7024554/il_570xN.473259184_iqm9.jpg",
@@ -37,17 +37,12 @@ public class ProductCardBuilderTest {
             .asList("https://www.etsy.com/listing/108648389/glass-beaded-colorful-flower-beads-white?ref=related-0",
                     "https://www.etsy.com/listing/108816901/ooak-glass-and-metal-beaded-anklet-navy?ref=related-2",
                     "https://www.etsy.com/listing/157474664/beach-anklet-beautiful-green-blue?ref=related-4");
-    final List<Size> sizes = new ArrayList<Size>();
+    final Map<String, String> sizes = new HashMap<String, String>();
     final List<String> options = Arrays
             .asList("example option a", 
                     "example option b",
                     "example option c");
-    final Float weight = 1.77f;
-    final String pattern = "alternating beads with stars";
-    final String condition = "new, handmade";
     final String model = "Bead Ankle Bracelet - 10 inch anklet";
-    final String material = "Glass Beads, Metal Clasp";
-    final Float shippingCost = 2.99f;
     final String appLinkIos = "jump://VerticalA/x123";
     final String appLinkAndroid = "market://search?q=pub:etsy";
     
@@ -59,7 +54,7 @@ public class ProductCardBuilderTest {
         
         colors.add(color);
         
-        sizes.add(new Size("Medium", "md")); 
+        sizes.put("md", "Medium"); 
     }
     
     private void testMinimalCardAttributes(ProductCard card){
@@ -79,10 +74,9 @@ public class ProductCardBuilderTest {
         Offer offer = new OfferBuilder(price).build();
         offers.add(offer);
         
-        ProductCardBuilder cardBuilder = new ProductCardBuilder(name, offers); 
+        ProductCardBuilder cardBuilder = new ProductCardBuilder(name, offers, url); 
         
         cardBuilder.description(description);
-        cardBuilder.url(url);
         cardBuilder.image(imgUrl);
         cardBuilder.brand(brand);
         
@@ -95,7 +89,6 @@ public class ProductCardBuilderTest {
         testMinimalCardAttributes(card);
         
         assertEquals("Merchant should match", merchant, card.getMerchant());
-        assertEquals("Gender should match", gender, card.getGender().toString().toLowerCase());
         assertEquals("Colors should match", colors, card.getColors());
         
         List<String> combinedImages = new ArrayList<String>();
@@ -111,12 +104,7 @@ public class ProductCardBuilderTest {
         assertEquals("Related Items should match", relatedItems, card.getRelatedItems());
         assertEquals("Sizes should match", sizes, card.getSizes());
         assertEquals("Options should match", options, card.getOptions());
-        assertEquals("Weight should match", weight, card.getWeight(), FLOAT_COMPARISON_EPSILON);
-        assertEquals("Pattern should match", pattern, card.getPattern());
-        assertEquals("Condition should match", condition, card.getCondition());
         assertEquals("Model should match", model, card.getModel());
-        assertEquals("Material should match", material, card.getMaterial());
-        assertEquals("Shipping cost should match", shippingCost, card.getShippingCost(), FLOAT_COMPARISON_EPSILON);
         assertEquals("App link ios should match", appLinkIos, card.getAppLinkIos());
         assertEquals("App link Android should match", appLinkAndroid, card.getAppLinkAndroid());
     }
@@ -127,14 +115,12 @@ public class ProductCardBuilderTest {
         Offer offer = new OfferBuilder(price).build();
         offers.add(offer);
         
-        ProductCardBuilder builder = new ProductCardBuilder(name, offers);
+        ProductCardBuilder builder = new ProductCardBuilder(name, offers, url);
 
         builder.description(description);
-        builder.url(url);
         builder.image(imgUrl);
         builder.brand(brand);
         builder.merchant(merchant);
-        builder.gender(gender);
         builder.colors(colors);
         builder.images(images);
         builder.rating(rating);
@@ -143,12 +129,7 @@ public class ProductCardBuilderTest {
         builder.relatedItems(relatedItems);
         builder.sizes(sizes);
         builder.options(options);
-        builder.weight(weight);
-        builder.pattern(pattern);
-        builder.condition(condition);
         builder.model(model);
-        builder.material(material);
-        builder.shippingCost(shippingCost);
         builder.appLinkIos(appLinkIos);
         builder.appLinkAndroid(appLinkAndroid);
         
@@ -157,5 +138,5 @@ public class ProductCardBuilderTest {
         testExtensiveCardAttributes(card);
     }
     
-    // TODO: test missing fields are invalid
+    // TODO: test validation
 }
