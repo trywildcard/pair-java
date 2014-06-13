840d7feec58f8efb4776d36e9d3b7e5c0a497821
Michael Garate <michaelgarate@trywildcard.com>
2014-06-13 11:05:15 -0400

    jackson handle lowercase enums

--- src/main/java/com/wildcard/model/MappingColor.java
+++ src/main/java/com/wildcard/model/MappingColor.java
@@ -1,7 +1,15 @@
 package com.wildcard.model;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum MappingColor {
     Beige, Black, Blue, Bronze, Brown, Gold, Green, Gray, Metallic,
     Multicolored, OffWhite, Orange, Pink, Purple, Red, Silver,
-    Transparent, Turquoise, White, Yellow
+    Transparent, Turquoise, White, Yellow;
+    
+
+    @JsonValue
+    public String getName(){
+        return name().toLowerCase();
+    }
 }
