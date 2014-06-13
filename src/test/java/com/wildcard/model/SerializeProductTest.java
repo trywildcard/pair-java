f4f979862a48f078b5e7bef6af34cbeffc147653
Michael Garate <michaelgarate@trywildcard.com>
2014-06-13 10:31:34 -0400

    add to SerializeProductTest

--- src/test/java/com/wildcard/model/SerializeProductTest.java
+++ src/test/java/com/wildcard/model/SerializeProductTest.java
@@ -2,17 +2,17 @@ package com.wildcard.model;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Iterator;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wildcard.testUtil.TestUtil;
 
 public class SerializeProductTest {
     ObjectMapper mapper = new ObjectMapper();
@@ -20,20 +20,29 @@ public class SerializeProductTest {
     @Before
     public void prepare(){
         // TODO: test null and empty fields
-        // mapper.setSerializationInclusion(Include.NON_NULL);
-        // mapper.setSerializationInclusion(Include.NON_EMPTY);
+        //mapper.setSerializationInclusion(Include.NON_NULL);
+        //mapper.setSerializationInclusion(Include.NON_EMPTY);
     }
     
     @Test
     public void testSerializeProductCard() throws URISyntaxException, JsonMappingException, IOException{
-        File file = new File(getClass().getResource("example_product_card.json").toURI());
         
-        JsonNode inputNode = mapper.readTree(file);
-       
-        ProductCard card = mapper.readValue(file, ProductCard.class);
-        
+        String inputString = TestUtil.readResourceAsString("example_product_card.json");
+        JsonNode inputNode = mapper.readTree(inputString);
+
+        ProductCard card = mapper.readValue(inputString, ProductCard.class);
         JsonNode outputNode = mapper.readTree(mapper.writeValueAsString(card));
         
-        assertEquals(inputNode, outputNode);
+        assertEquals("Expected inputNode and outputNode sizes to match", inputNode.size(), outputNode.size());
+
+        Iterator<String> inputItr = inputNode.fieldNames();
+        Iterator<String> outputItr = outputNode.fieldNames();
+        
+        while (inputItr.hasNext()){
+            String inFieldName = inputItr.next();
+            String outFieldName = outputItr.next();
+            
+            assertEquals("Expected " + inFieldName + " to match", inputNode.get(inFieldName), inputNode.get(outFieldName));
+        }
     }
 }
