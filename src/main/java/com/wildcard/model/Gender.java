dd8c0ba31e8763e4e19b040b8f9fbb62aa1e8e7f
Michael Garate <michaelgarate@trywildcard.com>
2014-06-13 11:05:15 -0400

    jackson handle lowercase enums

--- src/main/java/com/wildcard/model/Gender.java
+++ src/main/java/com/wildcard/model/Gender.java
@@ -1,5 +1,12 @@
 package com.wildcard.model;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Gender {
-    MALE, FEMALE, UNISEX
+    MALE, FEMALE, UNISEX;
+
+    @JsonValue
+    public String getName(){
+        return name().toLowerCase();
+    }
 }
