99e8ea242dcb5b264cc321469ecc1e1b4fea2e83
Michael Garate <michaelgarate@trywildcard.com>
2014-06-24 14:21:37 -0400

    rename search result url to cardUrl

--- Pair-Java/src/test/java/com/wildcard/pair/model/DummyProduct.java
+++ Pair-Java/src/test/java/com/wildcard/pair/model/DummyProduct.java
@@ -41,12 +41,15 @@ public class DummyProduct {
     final String appLinkIos = "jump://VerticalA/x123";
     final String appLinkAndroid = "market://search?q=pub:etsy";
     final String productId = "155021118";
+    final URL cardUrl;
 
     public DummyProduct() throws MalformedURLException{
         
         url = new URL("http://www.etsy.com/listing/155021118/awesome-4th-of-july-patriotic-red-white?ref=&sref=");
         imgUrl = new URL("http://img0.etsystatic.com/017/0/7024554/il_570xN.473259184_iqm9.jpg");
-        
+        cardUrl = new URL("http://www.etsy.com/product_cards/155021118");
+
+
         ProductColor color = new ProductColor("Magenta",
                 "Magenta color value",
                 new URL("http://https://www.etsy.com/swatches/magenta.jpg"),
