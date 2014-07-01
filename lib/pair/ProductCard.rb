5805e6655a62134c633af6983c4d796e27d6c596
Karthik Senthil <karthik@trywildcard.com>
2014-07-01 15:16:11 -0400

    added validations to fields for ProductCard

--- lib/pair/ProductCard.rb
+++ lib/pair/ProductCard.rb
@@ -8,16 +8,19 @@ require 'uri'
 	class ProductCard
         include ActiveModel::Validations
 
-		attr_accessor :cardType, :name, :webUrl, :productId, :offers, :merchant, :brand, :description, :colors, :images, :rating, :ratingScale, :ratingCount, :relatedItems, :referencedItems, :sizes, :options, :model, :appLinkIos, :appLinkAndroid
+		attr_accessor :cardType, :name, :webUrl, :productId, :merchant, :brand, :description, :colors, :images, :rating, :ratingScale, :ratingCount, :relatedItems, :referencedItems, :sizes, :options, :model, :appLinkIos, :appLinkAndroid
 
+		attr_reader :offers
        # validates_presence_of :cardType
         validates :cardType, presence: true, inclusion: {in: %w(product product_search) }
-		validates_presence_of :webUrl
+		validates :webUrl, presence: true
+		##todo URL regex validation
 		#validates :webUrl, format: { with: /\A#{URI::regexp}\z/, on: :create}
-		#validates_presence_of :name
+
 		validates :name, presence: true
 
-		
+		#validate :offers_valid
+
 
 		def initialize(attributes = {})
 			attributes.each do |name, value|
@@ -25,8 +28,29 @@ require 'uri'
 				puts name
 				puts value
 			end	
+
 		end
 
+		def offers=(offers)
+
+			if !offers.is_a?(Array)
+				errors.add(:geographicAvailability, 'Must be an array!')
+			end
+
+			if offers.nil? || offers.empty? 
+				errors.add(:offers, 'Must have atleast one offer!!')
+			end
+
+			tempOffer = Offer.new
+			for index in 0 ... offers.size
+				#it'd be nice to do Offer.new (offers[index]) so activemodel validation can do all the work for us
+				#tempOffer.validateOffer(offers[index])
+			end
+
+			self.offers = offers
+
+		end
+		
 		def writeAsJson
 			puts self.to_json
 		end 
