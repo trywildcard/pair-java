3c6e877e264df59d9a3a2a36fe0c8edc644f9ec8
Michael Garate <michaelgarate@trywildcard.com>
2014-07-07 18:07:28 -0400

    add properties file

--- src/main/java/com/trywildcard/pair/Pair.java
+++ src/main/java/com/trywildcard/pair/Pair.java
@@ -1,22 +1,40 @@
 package com.trywildcard.pair;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
 /**
  * Created by michaelgarate on 7/2/14.
  */
 public class Pair {
-    public static final String VERSION = "0.0.1";
 
-    public static boolean strictValidation = false;
+    private static Properties properties = new Properties();
+
+    public static void init(){
+        InputStream input = null;
+
+        try {
+            input = new FileInputStream("config.properties");
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
     public static String getVersion(){
-        return VERSION;
+        return properties.getProperty("pairVersion");
     }
 
     public static boolean getStrictValidation(){
-        return strictValidation;
+        return Boolean.valueOf(properties.getProperty("strictValidation"));
     }
 
     public static void setStrictValidation(boolean b){
-        strictValidation = b;
+        properties.setProperty("strictValidation", String.valueOf(b));
     }
 }
