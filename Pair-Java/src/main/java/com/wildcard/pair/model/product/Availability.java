8d2cb6ff00cb32d515c223fc781acd80db121643
Michael Garate <michaelgarate@trywildcard.com>
2014-07-07 15:25:08 -0400

    rename package to com.trywildcard.pair

--- Pair-Java/src/main/java/com/wildcard/pair/model/product/Availability.java
+++
@@ -1,8 +0,0 @@
-package com.wildcard.pair.model.product;
-
-/**
- * Defines availability options.
- */
-public enum Availability {
-    Discontinued, InStock, InStoreOnly, LimitedAvailability, OnlineOnly, OutOfStock, PreOrder, SoldOut;
-}
