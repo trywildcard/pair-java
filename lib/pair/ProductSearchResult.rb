83e3860fcae95d2ccdfce655598f74f7c8c86864
Karthik Senthil <karthik@trywildcard.com>
2014-07-07 15:22:55 -0400

    updating variable names / validation post code review for product search

--- lib/pair/ProductSearchResult.rb
+++ lib/pair/ProductSearchResult.rb
@@ -1,16 +1,16 @@
 
 require 'active_model'
 
-
+module PairSDK
 	class ProductSearchResult
         include ActiveModel::Validations
+        include ActiveModel::Serializers::JSON
 
-		attr_accessor :name, :price, :productURL, :imageURL
+		attr_accessor :name, :price, :product_url, :image_url
 
         validates :name, presence: true
-        validates :price, presence: true
-        #todo: URL Validation
-        validates :productURL, presence: true
+        validates :price, presence: true, numericality: {greater_than_or_equal_to: 0}
+        validates :product_url, presence: true
         
 		def initialize(attributes = {})
 			attributes.each do |name, value|
@@ -18,4 +18,17 @@ require 'active_model'
 			end	
 		end
 
+		def attributes
+			instance_values
+		end
+
+		def to_json
+			if self.valid?
+				ActiveSupport::JSON.encode(self.as_json)
+			else
+				raise "Offer is not valid - please remedy the following errors:" << self.errors.messages.to_s
+			end   
+		end 
+
 	end
+end
