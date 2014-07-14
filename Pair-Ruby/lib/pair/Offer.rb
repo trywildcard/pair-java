62debf73fd8b1a2e63b3c85d60578f6658e3bfe7
Karthik Senthil <karthik@trywildcard.com>
2014-07-14 18:05:28 -0400

    changed offer_id to underscore variable, adjusted geographic_availability setter method and added correct price validation now that Offer is using the Price object

--- Pair-Ruby/lib/pair/Offer.rb
+++ Pair-Ruby/lib/pair/Offer.rb
@@ -6,18 +6,18 @@ module PairSDK
         include ActiveModel::Validations
         include ActiveModel::Serializers::JSON
 
-    attr_accessor :price, :original_price, :shipping_cost, :description, :availability, :quantity, :gender, :weight, :weight_units, :offerId, :sale_start_date, :sale_end_date, :expiration_date
+    attr_accessor :price, :original_price, :shipping_cost, :description, :availability, :quantity, :gender, :weight, :weight_units, :offer_id, :sale_start_date, :sale_end_date, :expiration_date
 
     attr_reader :geographic_availability
 
-    validates :price, presence: true, numericality: {greater_than_or_equal_to: 0}
-    validates :original_price, allow_nil: true, numericality: {greater_than_or_equal_to: 0}
-    validates :shipping_cost, allow_nil: true, numericality: {greater_than_or_equal_to: 0}
+    validates :price, presence: true
     validates :description, allow_nil: true, length: {minimum: 1}
     validates :availability, allow_nil: true, inclusion: {in: %w(Discontinued InStock InStoreOnly LimitedAvailability OnlineOnly OutOfStock PreOrder SoldOut) }
     validates :gender, allow_nil: true, inclusion: {in: %w(male female unisex) }
     validates :weight, allow_nil: true, numericality: {greater_than_or_equal_to: 0}
     validates :quantity, allow_nil: true, numericality: {only_integer: true, greater_than_or_equal_to: 0} 
+
+    validate :validatePrices
  
     def initialize(attributes = {})
       attributes.each do |name, value|
@@ -29,13 +29,36 @@ module PairSDK
       instance_values
     end
 
-    def geographic_availability(geographic_availability)
+    def geographic_availability=(geographic_availability)
       if !geographic_availability.is_a?(Array)
         @geographic_availability = [geographic_availability]
       else
         @geographic_availability = geographic_availability
       end
     end
+
+    def validatePrices
+      if @price.nil? || !@price.is_a?(Price) || !@price.valid?
+        errors.add(:price, 'price cannot be nil and must be a valid Price object')
+        return
+      end
+
+      if !@original_price.nil?
+        if !@original_price.is_a?(Price) || !@original_price.valid?
+          errors.add(:original_price, 'Original Price must be a valid Price object')
+          return
+        end
+      end
+
+      if !@shipping_cost.nil?
+        if !@shipping_cost.is_a?(Price) || !@shipping_cost.valid?
+          errors.add(:shipping_cost, 'shipping cost must be a valid Price object')
+          return
+        end
+      end
+  
+    end
+
 
     #exclude validation fields in the JSON output
     def as_json(options={})
