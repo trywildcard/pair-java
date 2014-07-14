324afc9885f9fe12786970cc1b4ce50b2a97b5a3
Karthik Senthil <karthik@trywildcard.com>
2014-07-14 18:07:08 -0400

    adding Price to spec helper

--- Pair-Ruby/spec/spec_helper.rb
+++ Pair-Ruby/spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require_relative '../lib/pair/ProductCard'
 require_relative '../lib/pair/Offer'
+require_relative '../lib/pair/Price'
 require_relative '../lib/pair/ProductSearchCard'
 require_relative '../lib/pair/ProductSearchResult'
+
 
 require 'yaml'
