def5e288a61c70ad21058e8fc7224ed9d20e90bc
Karthik Senthil <karthik@trywildcard.com>
2014-07-31 10:32:49 -0400

    adding new Color object to spec helper

--- Pair-Ruby/spec/spec_helper.rb
+++ Pair-Ruby/spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/wildcard-pair/ProductCard'
 require_relative '../lib/wildcard-pair/Offer'
 require_relative '../lib/wildcard-pair/Price'
+require_relative '../lib/wildcard-pair/Color'
 require_relative '../lib/wildcard-pair/ProductSearchCard'
 require_relative '../lib/wildcard-pair/ProductSearchResult'
 
