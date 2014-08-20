ada8cf411d3aacf22f1b00248cc5cd5ade1e2e26
Karthik Senthil <karthik@trywildcard.com>
2014-08-20 10:30:33 -0400

    adding Color to the gem

--- Pair-Ruby/lib/wildcard-pair.rb
+++ Pair-Ruby/lib/wildcard-pair.rb
@@ -1,5 +1,6 @@
 require 'wildcard-pair/Price.rb'
 require 'wildcard-pair/Offer.rb'
+require 'wildcard-pair/Color.rb'
 require 'wildcard-pair/ProductCard.rb'
 require 'wildcard-pair/ProductSearchCard.rb'
 require 'wildcard-pair/ProductSearchResult.rb'
