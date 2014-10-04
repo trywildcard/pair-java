e38f622071b22717dfc2d76b03856606f7143544
Karthik Senthil <karthik@trywildcard.com>
2014-10-04 00:34:59 -0400

    adding new product class

--- Pair-Ruby/lib/wildcard-pair.rb
+++ Pair-Ruby/lib/wildcard-pair.rb
@@ -1,6 +1,7 @@
 require 'wildcard-pair/Price.rb'
 require 'wildcard-pair/Offer.rb'
 require 'wildcard-pair/Color.rb'
+require 'wildcard-pair/Product.rb'
 require 'wildcard-pair/ProductCard.rb'
 require 'wildcard-pair/ProductSearchCard.rb'
 require 'wildcard-pair/ProductSearchResult.rb'
