27f44888c35b16b6f0bb8c4a68f357e291082874
Karthik Senthil <karthik@trywildcard.com>
2014-10-07 18:05:41 -0400

    removing ruby code from java repo

--- Pair-Ruby/lib/wildcard-pair.rb
+++
@@ -1,8 +0,0 @@
-require 'wildcard-pair/Price.rb'
-require 'wildcard-pair/Offer.rb'
-require 'wildcard-pair/ProductCard.rb'
-require 'wildcard-pair/ProductSearchCard.rb'
-require 'wildcard-pair/ProductSearchResult.rb'
-
-module WildcardPair
-end
